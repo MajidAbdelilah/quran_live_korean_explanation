ffb85a1ac3be09eb38ebcfa5c5f1a8e9e74a7a4b
MajidAbdelilah <abdelilahmajid9@gmail.com>
2023-02-12 02:21:14 +0100

    adding app.js

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const { exec } = require('child_process');
+
+
+
+exec('git clone --depth 1 https://github.com/yt-dlp/yt-dlp.git && cd yt-dlp && ./yt-dlp.sh https://www.youtube.com/watch?v=8NBSwDEf8a8', (err, stdout, stderr) => {
+  if (err) {
+	console.log("err = "+err);
+    // node couldn't execute the command
+    return;
+  }
+
+  // the *entire* stdout and stderr (buffered)
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
 exec('ffmpeg', (err, stdout, stderr) => {
   if (err) {
 	console.log("err = "+err);
